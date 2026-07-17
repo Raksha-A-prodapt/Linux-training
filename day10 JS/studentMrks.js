4c6aa391e2b4dcffe85b88d419f72dd25a456175
Raksha-A-prodapt <raksha.a@prodapt.com>
2026-07-17 17:42:04 +1300

    Add files via upload

---
+++ day10 JS/studentMrks.js
@@ -0,0 +1,17 @@
+let marks=[85,42,76,91,38,67,55,29,80,49];
+let tp=0;
+let tf=0;
+for (let i=0;i<marks.length;i++ ){
+    if(marks[i]>=50){
+        // sconsole.log(`Student i: marks[i] - PASS`)
+         console.log("Student: "+i+" "+marks[i]+" - PASS");
+         tp++;
+    }else{
+        console.log("Student: "+i+" "+marks[i]+" - FAIL");
+        tf++;
+    }
+
+}
+
+console.log("Total Students Passed: "+tp);
+console.log("Total Student Failed: "+tf)
